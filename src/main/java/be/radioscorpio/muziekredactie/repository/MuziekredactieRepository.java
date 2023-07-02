f9027f7615495d18598d366b46f5a167ebbcd0df
Sam <depapa_33@yahoo.com>
2023-07-02 17:18:55 +0200

    added dummy list in repository

--- src/main/java/be/radioscorpio/muziekredactie/repository/MuziekredactieRepository.java
+++ src/main/java/be/radioscorpio/muziekredactie/repository/MuziekredactieRepository.java
@@ -1,12 +1,31 @@
 package be.radioscorpio.muziekredactie.repository;
 
 import be.radioscorpio.muziekredactie.domain.Lijst;
+import be.radioscorpio.muziekredactie.domain.LijstItem;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class MuziekredactieRepository {
 
     public Lijst getLijstById(String id) {
-        return new Lijst();
+
+        Lijst lijst = new Lijst();
+        lijst.setLijstId("1");
+
+        List<LijstItem> lijstItems = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            LijstItem lijstItem = new LijstItem();
+            lijstItem.setLijstId("1");
+            lijstItem.setLijstItemId(Integer.toString(i));
+            lijstItem.setArtiest("Artiest " + i);
+            lijstItem.setNummer("Nummer " + i);
+
+            lijstItems.add(lijstItem);
+        }
+        lijst.setLijstItems(lijstItems);
+        return lijst;
     }
 }
