a7392ce5d5051b6dbe7d05b75282a07d23864d2c
Sam <depapa_33@yahoo.com>
2023-07-02 13:01:17 +0200

    first commit

---
+++ src/main/java/be/radioscorpio/muziekredactie/MuziekredactieApplication.java
@@ -0,0 +1,13 @@
+package be.radioscorpio.muziekredactie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziekredactieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuziekredactieApplication.class, args);
+	}
+
+}
