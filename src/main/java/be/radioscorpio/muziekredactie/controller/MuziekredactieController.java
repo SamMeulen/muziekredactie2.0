907baf8f0fbacc51c92914666326a8816527c76b
Sam <depapa_33@yahoo.com>
2023-07-02 16:51:48 +0200

    refactoring controller

--- src/main/java/be/radioscorpio/muziekredactie/controller/MuziekredactieController.java
+++ src/main/java/be/radioscorpio/muziekredactie/controller/MuziekredactieController.java
@@ -3,6 +3,7 @@ package be.radioscorpio.muziekredactie.controller;
 import be.radioscorpio.muziekredactie.domain.Lijst;
 import be.radioscorpio.muziekredactie.service.interfaces.MuziekredactieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,6 @@ public class MuziekredactieController {
 
     @GetMapping("/lijst/{lijstId}")
     public ResponseEntity<Lijst> getTestData(String lijstId) {
-        Lijst lijst = muziekredactieService.getLijstById(lijstId);
-        return ResponseEntity.ok(lijst);
+        return new ResponseEntity<>(muziekredactieService.getLijstById(lijstId), HttpStatus.OK);
     }
 }
